f6aaa41ebda73b1289217c6f34fff348cd109c31
jeff-burns <1jeffreyburns@gmail.com>
2018-07-11 22:52:43 -0600

    added dogs list

---
+++ src/lib/API.js
@@ -0,0 +1,8 @@
+const API_URL = window.location.hostname == 'localhost' ? 'http://localhost:3000/api/v1/dogs' : 'https://dog-website-api.herokuapp.com/api/v1/dogs';
+
+export default {
+    async getAll() {
+        const response = await fetch(API_URL);
+        return await response.json();
+    }
+}
